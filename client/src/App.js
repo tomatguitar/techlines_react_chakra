9b67d06f98450a91fde0a4cea6b739ded86f66a6
Dmytro Zhyltsov <tomatguitar@gmail.com>
2024-01-29 19:09:05 +0200

    Added ProductsScreen and ProductCard

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,18 @@
-import {ChakraProvider} from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import ProductsScreens from "./screens/ProductsScreens";
 
 function App() {
   return (
     <ChakraProvider>
       <Router>
         <Navbar />
-        <main>{/* <Routes></Routes> */}</main>
+        <main>
+          <Routes>
+            <Route path="/products" element={<ProductsScreens />}></Route>
+          </Routes>
+        </main>
       </Router>
     </ChakraProvider>
   );
---
+++ client/src/screens/ProductsScreens.jsx
@@ -0,0 +1,18 @@
+import { Center, Wrap, WrapItem  } from "@chakra-ui/react";
+import { products } from './../products';
+import ProductCard from "../components/ProductCard";
+const ProductsScreens = () => {
+  return (
+    <Wrap spacing='30px' justify='center' minHeight='100vh'>
+        {products.map((product) => (
+            <WrapItem key={product._id}>
+                <Center w='250px' h='550px'>
+                    <ProductCard product={product}></ProductCard>
+                </Center>
+            </WrapItem>
+        ))}
+    </Wrap>
+  );
+}
+
+export default ProductsScreens;
